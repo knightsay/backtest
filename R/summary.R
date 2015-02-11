0abada767c303f0c2a08fbd5f0166a2df1e24341
knightsay <knightsay@hotmail.com>
2015-02-10 21:57:11 -0500

    relocate bt.sharpe in to summary.R since only summary.R uses it

--- R/summary.R
+++ R/summary.R
@@ -78,3 +78,26 @@ setMethod("summary",
             
           }
 )
+
+
+## ".bt.sharpe" Returns a one-dimensional array of sharpe ratios.
+## It is put here because summary-methods is the only place calling it
+
+.bt.sharpe <- function(object){
+  
+  stopifnot(object@natural)
+  
+  ## creates the array to store the sharpe ratios
+  
+  sr <- array(dim = c(1, length(object@in.var)),
+              dimnames = list("sharpe", object@in.var))
+  
+  ## handles single or multiple "in.var"
+  
+  for(i in object@in.var){
+    x <- object@results[object@ret.var, i, , ,"means"]
+    spreads <- x[,"high"] - x[,"low"]
+    sr[1,i] <- mean(spreads, na.rm = TRUE)/sd(spreads, na.rm = TRUE)
+  }
+  sr
+}
